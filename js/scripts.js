362afa428d9704542ba93df1817f60ea6b70fb26
geomio <geoff.millner22@gmail.com>
2021-03-23 10:46:53 -0700

    made html form area and basic js if if else logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
-  $("form#quiz").submit(function(event) {
-    
+// $(document).ready(function() {
+//   $("form#quiz").submit(function(event) {
 
-    event.preventDefault();
-  });
-});
+
+//     event.preventDefault();
+//   });
+// });
+
+let answer = 31
+
+if(answer <= 10 && answer >= 1){
+  console.log("answer1");
+}else if(answer <=20 && answer >= 11){
+  console.log("answer2");
+}else if(answer <=30 && answer >= 21){
+  console.log("answer3");
+}else if(answer <=40 && answer >= 31){
+  console.log("answer4");
+}
